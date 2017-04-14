58c2a21c0e46c26b25fe03b5cdc3b98edea4c27e
Marijn Rombouts <mpgrombouts@gmail.com>
2017-04-14 17:02:29 +0000

    Add display driver

--- display.h
+++ display.h
@@ -1,7 +1,25 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
+#define NO_DISPLAYS 6
+
+#include <avr/interrupt.h>
 #include "shiftreg.h"
 
+typedef unsigned char uint8_t;
+
+// Shift registers
+shiftreg displayreg;
+shiftreg segmentreg;
+
+// Display buffers
+static uint8_t disp[NO_DISPLAYS];
+static uint8_t currentDisplay;
+
+void display_init();
+void display_set(uint8_t dispID, uint8_t content);
+
+static void next();
+static uint8_t toSegment(char input);
 
 #endif
